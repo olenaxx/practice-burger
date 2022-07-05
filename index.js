b6f6aeeff61941ee4ab70c22831a76d4bc3759a4
Olena Stadnyk <olena.lightness@gmail.com>
2022-07-05 07:54:51 +0300

    feat: add slider

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const hamburger_menu = document.querySelector('.hamburger-menu');
 const navbar = document.querySelector('header nav');
 const links = document.querySelectorAll('.links')
 
+/*----------------- burger-menu ------------------*/
+
 function closeMenu() {
   navbar.classList.remove('open');
   document.body.classList.remove('stop-scrolling');
@@ -17,3 +19,65 @@ hamburger_menu.addEventListener('click', () => {
 })
 
 links.forEach(link=>link.addEventListener('click', () => closeMenu()))
+
+/*----------------- slider ------------------*/
+
+const prev = document.getElementById('btn-prev')
+const next = document.getElementById('btn-next')
+const slides =document.querySelectorAll('.slide')
+// const slides =[...document.querySelectorAll('.slide')]
+const dots = document.querySelectorAll('.dot')
+
+let index = 0;
+// console.log(slides);
+
+const activeSlide = n => {
+  for (slide of slides) {
+    slide.classList.remove('active')
+  }
+  slides[n].classList.add('active')
+}
+
+const activeDot = n => {
+  for (dot of dots) {
+    dot.classList.remove('active')
+  }
+  dots[n].classList.add('active')
+}
+
+const prepareCurrentSlide = ind => {
+  activeSlide(ind);
+  activeDot(ind);
+}
+
+const nextSlide = () => {
+  if (index === slides.length - 1) {
+    index = 0;
+    prepareCurrentSlide(index);
+  } else {
+    index++;
+    prepareCurrentSlide(index);
+  }
+}
+
+const prevSlide = () => {
+  if (index === 0) {
+    index = slides.length - 1;
+    prepareCurrentSlide(index);
+  } else {
+    index--;
+    prepareCurrentSlide(index);
+  }
+}
+
+dots.forEach((item, indexDot) => {
+  item.addEventListener('click', () => {
+    index = indexDot;
+    prepareCurrentSlide(index);
+  })
+})
+
+next.addEventListener('click', nextSlide)
+prev.addEventListener('click', prevSlide)
+
+// setInterval(nextSlide, 2500)
